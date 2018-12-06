97a123c55519b887c686f5578568339e01a97628
hsm-hx <skytea922@gmail.com>
2018-12-06 18:23:58 +0900

    :construction: Editページを追加 #5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,6 +33,34 @@ class TasksController < ApplicationController
       end
     end
   end
+  def edit
+    set_task
+  end
+  def update
+    set_task
+
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html{
+          redirect_to @task,
+          notice: "Task was successfully updated."
+        }
+        format.json{
+          render :show,
+          status: :ok,
+          location: @task
+        }
+      else
+        format.html{
+          render :edit
+        }
+        format.json{
+          render json: @task.errors,
+          status: :unprocessable_entity
+        }
+      end
+    end
+  end
 
   private
     def set_task
