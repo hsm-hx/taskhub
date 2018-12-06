b4ca5c7814f0230983f4daeedd68699fd2c0aa4b
hsm-hx <skytea922@gmail.com>
2018-12-06 18:01:45 +0900

    :construction: Showページを追加 #5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,12 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  def show
+    set_task
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
 end
