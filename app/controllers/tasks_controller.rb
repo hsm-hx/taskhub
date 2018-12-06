b8c340c63e2630cfad6b5a30fe5b256ff4a34aa9
hsm-hx <skytea922@gmail.com>
2018-12-06 18:21:02 +0900

    :construction: Editページを追加 #5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,40 @@ class TasksController < ApplicationController
   def show
     set_task
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html{
+          redirect_to @task,
+          notice: "Task was successfully created."
+        }
+        format.json{
+          render :show,
+          status: :created,
+          location: @task
+        }
+      else
+        format.html{
+          render :new
+        }
+        format.json{
+          render json: @task.errors,
+          status: :unprocessable_entity
+        }
+      end
+    end
+  end
 
   private
     def set_task
       @task = Task.find(params[:id])
+    end
+    def task_params
+      params.require(:task).permit(:title, :description, :user_id, :folder_id, :label_id, :due_day)
     end
 end
