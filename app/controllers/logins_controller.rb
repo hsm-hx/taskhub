8099043e759e5358c86754dae34ed1d933766685
hsm-hx <skytea922@gmail.com>
2018-12-10 23:17:24 +0900

    :bug: ログイン実装したけど何故かパスワード認証ができない

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,2 +1,23 @@
 class LoginsController < ApplicationController
+  def show
+    render "new"
+  end
+
+  def create
+    user = User.find_by(user_id: params[:user_id])
+
+    if user #and user.authenticate(params[:pass])
+      session[:user_id] = user.id
+      redirect_to tasks_url
+    else 
+      flash.now.alert = "ユーザーIDまたはパスワードが間違っています"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @current_user = nil
+    redirect_to login_path
+  end
 end
